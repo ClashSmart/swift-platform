440a81a009332a75f245d8b2ce6bd047c6b7f018
HarryHong <hong091114@gmail.com>
2015-02-04 02:03:02 -0500

    Initial the task

---
+++ ClashSmart/ClashSmart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClashSmart
+//
+//  Created by honghao on 2015-02-04.
+//  Copyright (c) 2015 honghao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
